a22f1ffad6491c7f85fa81f9e76d493bfa6d7603
Dev ;) <devbhagavan25@gmail.com>
2024-04-17 23:20:49 +0530

    renamed login.css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
+import LoginForm from './components/login';
 
 function App() {
+  return(<LoginForm/>);
 }
 export default App;
--- src/components/login.js
+++ src/components/login.js
@@ -1,16 +1,8 @@
 import React from "react";
+import "../assets/css/login.css"
 
 function LoginForm(){
-    return(<div><!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <link href="login2.CSS" rel="stylesheet" type="text/CSS" />
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Library Management System</title>
-        </head>
-        <body>
+    return(
             <div class="main">
                 <div class="box">
                     <div class="title">
@@ -34,7 +26,6 @@ function LoginForm(){
                 </div>
                 
             </div>
-        </body>
-        </html></div>);
+    );
 }
 export default LoginForm;
