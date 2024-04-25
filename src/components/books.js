355b8c646155f06fbe361df4e7f3ae384f040cd1
Dev ;) <devbhagavan25@gmail.com>
2024-04-25 23:45:51 +0530

    Edit Book Done

--- src/components/books.js
+++ src/components/books.js
@@ -1,14 +1,89 @@
-import React from "react"; 
-
+import React, { useContext } from "react"; 
+import { UserContext } from "../context/context";
 import { useState ,useEffect} from "react";
 import { BookRef,BookGenreRef } from "../context/DBContext";
-import { getDocs,deleteDoc,doc,getCountFromServer,query,where,or, orderBy } from "firebase/firestore";
+import { getDocs,deleteDoc,doc,getCountFromServer,query,where,or, orderBy,updateDoc } from "firebase/firestore";
 import Popup from "reactjs-popup";
 import AddBook from "./addBook";
 
 
 import "../assets/css/books.css";
 
+
+function EditBook(props){
+    
+
+    const [bookName,setBookName] = useState(props.BookName);
+    const [authorName,setAuthorName] = useState(props.author);
+    const [genre,setGenre] = useState("");
+    const [availability,setAvailability] = useState(props.avail);
+    const [BookID,setBookID] = useState(props.bookID);
+    const [genreID,setGenreID] = useState("");
+    const id = props.id;
+
+    console.log(Number(BookID))
+    const getGenre = async()=>{
+        const q = query(BookGenreRef,where("BookID","==",Number(BookID)));
+        try{
+            const data = await getDocs(q);
+            data.forEach((doc)=>{
+                setGenre(doc.data().Genre)
+                setGenreID(doc.id);
+            })
+        }catch(err){
+            console.log(err)
+        }
+    }   
+
+    const updateBook = async (e) =>{
+        e.preventDefault();
+        const b = doc(BookRef,id);
+        const g = doc(BookGenreRef,genreID);
+        try{
+            await updateDoc(b,{
+                BookName : bookName,
+                Author : authorName,
+                BookAvailability : Number(availability)}
+            )
+            await updateDoc(g,{
+                Genre : genre,
+            })
+
+            alert("Book Updated Successfully")
+        }catch(err){
+            console.log(err)
+        }
+
+    }
+
+    useEffect(()=>{
+        getGenre();
+    },[])
+
+    return(
+        <div class="book-form">
+            <link
+            href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
+            rel="stylesheet"
+            />
+            <h2>Book Information</h2>
+            <form id="bookForm">
+                <label for="bookName">Book Name:</label>
+                <input type="text" id="bookName" placeholder="Enter Book Name" name="bookName" value={bookName} onChange={(e)=>{setBookName(e.target.value)}} required />
+                
+                <label for="author">Author:</label>
+                <input type="text" id="author" placeholder="Enter Author Name" name="author" value={authorName} required onChange={(e)=>{setAuthorName(e.target.value)}} />
+                
+                <label for="genre">Genre:</label>
+                <input type="text" id="genre" placeholder="Enter Genre" name="genre" value={genre} required onChange={(e)=>{setGenre(e.target.value)}} />
+                
+                <label for="availability">Availability:</label>
+                <input type="number" id="availability" placeholder="Enter Availability" name="availability" value={availability} required onChange={(e)=>{setAvailability(e.target.value)}} />
+                <input type="submit" onClick={(e)=>{updateBook(e)}} value="Submit"  />
+            </form>
+        </div>
+    );
+}
 
 
 function GetGenre(props){
@@ -23,7 +98,7 @@ function GetGenre(props){
     useEffect(()=>{
         fetchGenre();
     },[])
-    return(<td>{genre}</td>);
+    return(genre);
 }
 
 function Books(){
@@ -155,11 +230,11 @@ function Books(){
                                     <td>{doc.BookName}</td>
                                     <td>{doc.LibID}</td>
                                     <td>{doc.BookAvailability}</td>
-                                    <GetGenre BookID={doc.BookID}/>
+                                    <td><GetGenre BookID={doc.BookID}/></td>
                                    {/*<td>{doc.ReturnBookDate&&doc.ReturnBookDate.toDate()&&doc.ReturnBookDate.toDate().toString()}</td>*/}
                                     <td id="fn-btn">
                                         <button class="delete-btn" onClick={()=>{deleteBooks(doc.id,doc.BookID)}}><i class='bx bxs-trash'></i></button>
-                                        <button class="edit-btn"><i class='bx bxs-pencil'></i></button>
+                                        <Popup trigger={<button class="edit-btn"><i class='bx bxs-pencil'></i></button>} position="left"><EditBook id={doc.id} bookID={doc.BookID} author={doc.Author} avail={doc.BookAvailability} BookName={doc.BookName}  /></Popup>
                                     </td>
                                 </tr>
 
