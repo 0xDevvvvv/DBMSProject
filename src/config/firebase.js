72d548537e11ceee0fd10885def390954e1a3455
Dev ;) <128991401+0xDevvvvv@users.noreply.github.com>
2024-05-21 21:26:58 +0530

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -7,12 +7,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBxIP3R1Fgzamj98rs9xUcdnmuFh4GH1VQ",
-  authDomain: "dbmsproject-6690a.firebaseapp.com",
-  projectId: "dbmsproject-6690a",
-  storageBucket: "dbmsproject-6690a.appspot.com",
-  messagingSenderId: "426479962710",
-  appId: "1:426479962710:web:c2d4b61e1e74712793c9f7"
+// add firebase api key
 };
 
 // Initialize Firebase
